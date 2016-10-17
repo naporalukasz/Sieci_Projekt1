4bd935cbd204f44c1659ff42b87a8e23664dea58
plodczyks <slawekplodczyk@onet.eu>
2016-10-17 22:42:18 +0200

    app config changes - test commit

--- Sieci_Projekt1/Program.cs
+++ Sieci_Projekt1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +12,77 @@ namespace Sieci_Projekt1
     {
         static void Main(string[] args)
         {
+            int countInput,countOutput,iterationsCount;
+            List<int> layers = new List<int>();
+            FunctionTypeEnum functionType;
+            ProblemTypeEnum problemType;
+            bool bias;
+            double learningCoefficient, inertiaCoefficient;
+
+            #region Handle configuration
+
+            var countInputString = ConfigurationManager.AppSettings["CountInput"];
+            if (!int.TryParse(countInputString, out countInput) || countInput<=0) { HandleError("CountInput"); return; }
+
+            var countOutputString = ConfigurationManager.AppSettings["CountOutput"];
+            if (!int.TryParse(countOutputString, out countOutput) || countOutput<=0) { HandleError("CountOutput"); return; }
+
+            var iterationsCountString = ConfigurationManager.AppSettings["IterationsCount"];
+            if (!int.TryParse(iterationsCountString, out iterationsCount) || iterationsCount <= 0) { HandleError("IterationsCount"); return; }
+
+            var layerString = ConfigurationManager.AppSettings["Layers"];
+            var layerStrings = layerString.Split(',');
+            foreach(var layerStr in layerStrings)
+            {
+                int layerValue;
+                if (!int.TryParse(layerStr, out layerValue) || layerValue <= 0) { HandleError("Layers"); return; }
+                else { layers.Add(layerValue); }
+            }
+
+            var functionTypeString = ConfigurationManager.AppSettings["FunctionType"];
+            if (functionTypeString == "Bipolar") functionType = FunctionTypeEnum.Bipolar;
+            else if (functionTypeString == "Unipolar") functionType = FunctionTypeEnum.Unipolar;
+            else { HandleError("FunctionType"); return; }
+
+            var problemTypeString = ConfigurationManager.AppSettings["ProblemType"];
+            if (problemTypeString == "Classification") problemType = ProblemTypeEnum.Classification;
+            else if (problemTypeString == "Regression") problemType = ProblemTypeEnum.Regression;
+            else { HandleError("ProblemType"); return; }
+
+            var biasString = ConfigurationManager.AppSettings["Bias"];
+            if (biasString == "true") bias = true;
+            else if (biasString == "false") bias = false;
+            else { HandleError("Bias"); return; }
+
+            var learningCoefficientString = ConfigurationManager.AppSettings["LearningCoefficient"];
+            if (!double.TryParse(learningCoefficientString,NumberStyles.AllowDecimalPoint,new CultureInfo("en-US"), out learningCoefficient) || learningCoefficient <= 0 || learningCoefficient >= 1) { HandleError("LearningCoefficient"); return; }
+
+            var inertiaCoefficientString = ConfigurationManager.AppSettings["InertiaCoefficient"];
+            if (!double.TryParse(inertiaCoefficientString, NumberStyles.AllowDecimalPoint, new CultureInfo("en-US"), out inertiaCoefficient) || inertiaCoefficient <= 0 || inertiaCoefficient >= 1) { HandleError("InertiaCoefficient"); return; }
+            #endregion
 
             var parameters = new Parameters
             {
-                Layers = new List<int> { 5,5 },
-                CountInput =2,
-                CountOutput = 1,
-                FunctionType = FunctionTypeEnum.Bipolar,
-                Bias = true,
-                IterationsCount = 5000,
-                LearingCoefficient = 0.2,
-                InertiaCoefficient = 0.9,
-                ProblemType = ProblemTypeEnum.Classification
+                Layers = layers,
+                CountInput =countInput,
+                CountOutput = countOutput,
+                FunctionType = functionType,
+                Bias = bias,
+                IterationsCount = iterationsCount,
+                LearingCoefficient = learningCoefficient,
+                InertiaCoefficient = inertiaCoefficient,
+                ProblemType = problemType
             };
 
             var problem = new Problem(parameters);
             problem.Execute();
 
+        }
 
-           // Console.ReadKey();
+        private static void HandleError(string settingName)
+        {
+            Console.WriteLine("Wrong data for property " + settingName);
+            Console.ReadKey();
         }
     }
 }
